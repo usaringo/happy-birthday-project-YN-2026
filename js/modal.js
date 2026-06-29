f959d308b7f3d9ddaab119e05b4fe1daedfb7087
usaringo <102206897+usaringo@users.noreply.github.com>
2026-06-29 17:47:27 +0900

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -6,8 +6,35 @@ function createNoButtons(){
 }
 
 function showImageModal(){
-    modal.style.display="block";
-}
+
+    modal.innerHTML = `
+        <div id="modalContent">
+
+            <div id="close">×</div>
+
+            <h2>
+                緊急速報
+            </h2>
+
+            <img
+                src="images/pic1.jpg"
+                style="
+                    width:100%;
+                    max-width:500px;
+                "
+            >
+
+        </div>
+    `;
+
+    modal.style.display = "block";
+
+    document
+        .getElementById("close")
+        .addEventListener(
+            "click",
+            closeImageModal
+        );
 
 function closeImageModal(){
     modal.style.display="none";
