43dbe95a7d5131b0488bd50007cc8a4165eaaab9
usaringo <102206897+usaringo@users.noreply.github.com>
2026-06-29 16:27:16 +0900

    Create events.js

---
+++ js/events.js
@@ -0,0 +1,21 @@
+function eventCheck(){
+
+    switch(count){
+
+        case 3:
+            unlockNo();
+            break;
+
+        case 6:
+            showImageModal();
+            break;
+
+        case 8:
+            convertToYesNo();
+            break;
+
+        case 10:
+            finalMode();
+            break;
+    }
+}
