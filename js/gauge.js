da5f31bfe49e93411fa13afb52b3710d67c1ac55
usaringo <102206897+usaringo@users.noreply.github.com>
2026-08-27 17:31:11 +0900

    Update gauge.js

--- js/gauge.js
+++ js/gauge.js
@@ -1,7 +1,55 @@
-function startCharge() {
-    console.log("charge start");
+let gaugeValue = 0;
+let charging = false;
+
+function startCharge(){
+
+    gaugeValue = 0;
+    charging = true;
+
+    updateGauge();
+
+    document.addEventListener(
+        "touchstart",
+        chargeGauge
+    );
 }
 
-function stopCharge() {
-    console.log("charge stop");
+function chargeGauge(e){
+
+    if(!charging) return;
+
+    gaugeValue += 10;
+
+    if(gaugeValue >= 100){
+
+        gaugeValue = 100;
+
+        updateGauge();
+
+        stopCharge();
+
+        return;
+    }
+
+    updateGauge();
+}
+
+function updateGauge(){
+
+    document.getElementById("gauge")
+        .style.height = gaugeValue + "%";
+}
+
+function stopCharge(){
+
+    charging = false;
+
+    document.removeEventListener(
+        "touchstart",
+        chargeGauge
+    );
+
+    document.getElementById("chargeText")
+        .textContent =
+        "MAX！！";
 }
