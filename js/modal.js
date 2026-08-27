0d1853737618b50f53a34f83b0b48ed20bcf8971
usaringo <102206897+usaringo@users.noreply.github.com>
2026-08-27 17:23:27 +0900

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -3,24 +3,37 @@ const modal =
 
 function createNoButtons(){
 
-    // すでに作られている複製を削除
+    // 追加された「いいえ」を削除
     document
         .querySelectorAll(".noClone")
         .forEach(el => el.remove());
 
-    // いいえを2個複製
+    // 「いいえ」を2個複製
     for(let i = 0; i < 2; i++){
 
         const clone = no.cloneNode(true);
 
-        clone.id = "";
+        // IDを削除
+        clone.removeAttribute("id");
 
+        // 複製用クラス
         clone.classList.add("noClone");
+
+        // 元のボタンと同じ見た目にする
+        clone.style.position = "fixed";
+        clone.style.padding = "15px 30px";
+        clone.style.fontSize = "20px";
+        clone.style.border = "none";
+        clone.style.borderRadius = "10px";
+        clone.style.background = "";
+        clone.style.color = "";
 
         document.body.appendChild(clone);
 
+        // ランダムな場所へ
         move(clone);
 
+        // 押したらゲームオーバー
         clone.addEventListener(
             "touchstart",
             noTouched
