e799ee9cf6f09b39180ac1aac39abd3d8823c9bb
usaringo <102206897+usaringo@users.noreply.github.com>
2026-08-31 16:09:14 +0900

    Update stackGame.js

--- js/stackGame.js
+++ js/stackGame.js
@@ -1,15 +1,29 @@
+const maxButton =
+    document.getElementById("maxButton");
+
+maxButton.addEventListener(
+    "touchstart",
+    function(e){
+
+        e.preventDefault();
+
+        startStackGame();
+    }
+);
+
 function startStackGame(){
 
     console.log("柱積みゲーム開始！");
 
-    // ゲージを隠す
     document.getElementById("gaugeBox")
         .style.display = "none";
 
     document.getElementById("chargeText")
         .style.display = "none";
 
-    // 柱積みゲームを表示
+    document.getElementById("maxButton")
+        .style.display = "none";
+
     document.getElementById("stackGame")
         .style.display = "block";
 }
