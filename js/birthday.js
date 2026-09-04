3b44b41c754cf62ce584b92d044cdab204cfe707
usaringo <102206897+usaringo@users.noreply.github.com>
2026-09-04 21:26:15 +0900

    Create birthday.js

---
+++ js/birthday.js
@@ -0,0 +1,76 @@
+const photoModal =
+    document.getElementById("photoModal");
+
+const modalImage =
+    document.getElementById("modalImage");
+
+const modalMessage =
+    document.getElementById("modalMessage");
+
+const modalClose =
+    document.getElementById("modalClose");
+
+
+/* =========================
+   画像をタップ
+========================= */
+
+document
+    .querySelectorAll(".photoCard img")
+    .forEach(function(img){
+
+        img.addEventListener(
+            "click",
+            function(){
+
+                modalImage.src =
+                    img.src;
+
+                modalMessage.textContent =
+                    img.dataset.message;
+
+                photoModal.style.display =
+                    "block";
+
+            }
+        );
+
+    });
+
+
+/* =========================
+   閉じる
+========================= */
+
+modalClose.addEventListener(
+    "click",
+    function(){
+
+        photoModal.style.display =
+            "none";
+
+        modalImage.src = "";
+
+    }
+);
+
+
+/* =========================
+   背景タップでも閉じる
+========================= */
+
+photoModal.addEventListener(
+    "click",
+    function(e){
+
+        if(e.target === photoModal){
+
+            photoModal.style.display =
+                "none";
+
+            modalImage.src = "";
+
+        }
+
+    }
+);
